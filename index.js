7a7ded34638be2c8fe19e6ad113ea7c393251d94
neil_hsieh <neil605164@gmail.com>
2021-08-21 12:41:48 +0800

    [add] task list use vue vFor to show list

---
+++ index.js
@@ -0,0 +1,47 @@
+var app = new Vue({
+    el: "#main",
+    data: {
+        title: "尚未存在任務清單",
+        taskList: [{
+                id: 1,
+                title: "task one",
+                finish: false,
+            },
+            {
+                id: 2,
+                title: "task two",
+                finish: false,
+            },
+            {
+                id: 3,
+                title: "task three",
+                finish: false,
+            },
+            {
+                id: 4,
+                title: "task four",
+                finish: false,
+            },
+            {
+                id: 5,
+                title: "task five",
+                finish: false,
+            },
+            {
+                id: 6,
+                title: "task six",
+                finish: false,
+            },
+            {
+                id: 7,
+                title: "task seven",
+                finish: false,
+            },
+            {
+                id: 8,
+                title: "task eight",
+                finish: false,
+            },
+        ],
+    }
+})
