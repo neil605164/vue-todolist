3e2904645ed3d998900b2101f9a00296a7afaff9
neil_hsieh <neil605164@gmail.com>
2021-08-28 14:27:29 +0800

    [add] delete all and one function

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ var app = new Vue({
         title: "尚未存在任務清單",
         taskName: "",
         taskList: [],
-        newTaskList: [],
         taskEnevt: "ALL",
     },
     methods: {
         addTask() {
             vm = this
+
+            // 當送入空資料時，不理會
+            if (vm.taskName === "") {
+                return
+            }
+
             obj = {
                 id: Date.now(),
                 title: vm.taskName,
@@ -21,23 +26,38 @@ var app = new Vue({
             vm.taskName = ""
             return
         },
+        deleteAll() {
+            return this.taskList = []
+        },
+        deleteOne(item) {
+
+            // 檢查傳入內容跟清單內容是否相同
+            index = this.taskList.findIndex(function(task, key) {
+
+                console.log(task.id, item.id)
+                return task.id === item.id
+            })
+
+            this.taskList.splice(index, 1);
+
+            return
+        }
     },
     computed: {
         // 取全部的任務清單
         TaskList() {
             vm = this
             if (vm.taskEnevt == "ALL") {
-                vm.newTaskList = vm.taskList
+                return vm.taskList
             }
 
             if (vm.taskEnevt == "Doing") {
-                vm.newTaskList = vm.taskList.filter(item => item.finish == false)
+                return vm.taskList.filter(item => item.finish == false)
             }
 
             if (vm.taskEnevt == "Done") {
-                vm.newTaskList = vm.taskList.filter(item => item.finish == true)
+                return vm.taskList.filter(item => item.finish == true)
             }
-            return this.newTaskList
         }
     }
 })
